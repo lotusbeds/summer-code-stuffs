f4deee51c6aab27a0e43476f0f5fdc07ede66f3f
lotusbeds <87618525+lotusbeds@users.noreply.github.com>
2021-07-22 13:31:47 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# summer-code-stuffs
-some things i coded over the summer haha
+- 👋 Hi, I’m @lotusbeds
+- 👀 I’m interested in ... your mom
+- 🌱 I’m currently learning ... python :D
+- 💞️ I’m looking to collaborate on ... huh
+- 📫 How to reach me ... uhhh idk
+
+<!---
+lotusbeds/lotusbeds is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
---
+++ connect4.py
@@ -0,0 +1,97 @@
+board = [[' _ ', ' _ ', ' _ ', ' _ ', ' _ ', ' _ ', ' _ '], 
+        [' _ ', ' _ ', ' _ ', ' _ ', ' _ ', ' _ ', ' _ '], 
+        [' _ ', ' _ ', ' _ ', ' _ ', ' _ ', ' _ ', ' _ '], 
+        [' _ ', ' _ ', ' _ ', ' _ ', ' _ ', ' _ ', ' _ '], 
+        [' _ ', ' _ ', ' _ ', ' _ ', ' _ ', ' _ ', ' _ '], 
+        [' _ ', ' _ ', ' _ ', ' _ ', ' _ ', ' _ ', ' _ '], 
+        [' _ ', ' _ ', ' _ ', ' _ ', ' _ ', ' _ ', ' _ ']]
+
+cols = [6, 6, 6, 6, 6, 6, 6]
+won = False 
+x = 0
+y = 0
+
+def makeBoard():
+    for x in range(len(board)):
+        for y in range(len(board[0])):
+            print(board[x][y], end= "")
+        print("")
+
+def chooseLetter():
+    while True:
+        print("Player 1, do you want to be 'X' or 'O'?")
+        letter = input("")
+        if letter == "X":
+            print("Ok! You're 'X'!")
+            return ' X ', ' O '
+        elif letter == "O":
+            print("Ok! You're 'O'!")
+            return ' O ', ' X '
+        else:
+            print("Please type in 'O' or 'X'! [Case sensitive.]")
+
+def p1move():
+    print("Player 1: what column do you want to put your piece?")
+    y = int(input(""))
+    x = cols[y]
+    while True:
+        (board[x][y]) = p1Symbol
+        cols[y] = cols[y]-1 
+        makeBoard()
+        break       
+
+def p2move():
+    print("Player 2: what column do you want to put your piece?")
+    y = int(input(""))
+    x = cols[y]
+    while True:
+        (board[x][y]) = p2Symbol
+        cols[y] = cols[y]-1
+        makeBoard()
+        break
+
+def p1won():
+    print("Player 1 won!")
+    exit()
+    
+
+def p2won():
+    print("Player 2 won!")
+    exit()
+
+def checkWin():
+    for x in range(len(board)):
+        for y in range(3):
+            if (board[x][y]) == p1Symbol and (board[x][y+1]) == p1Symbol and (board[x][y+2]) == p1Symbol and (board[x][y+3]) == p1Symbol:
+                p1won()
+    for x in range(len(board)):
+        for y in range(3):
+            if (board[x][y]) == p2Symbol and (board[x][y+1]) == p2Symbol and (board[x][y+2]) == p2Symbol and (board[x][y+3]) == p2Symbol:
+                p2won()
+    for x in range(4):
+        for y in range(len(board)):
+            if (board[x][y]) == p2Symbol and (board[x+1][y]) == p2Symbol and (board[x+2][y]) == p2Symbol and (board[x+3][y]) == p2Symbol:
+                p2won()
+    for x in range(4):
+        for y in range(len(board)):
+            if (board[x][y]) == p1Symbol and (board[x+1][y]) == p1Symbol and (board[x+2][y]) == p1Symbol and (board[x+3][y]) == p1Symbol:
+                p1won()
+    for x in range(len(board)):
+        for y in range(3):
+            if (board[x][y]) == p1Symbol and (board[x+1][y+1]) == p1Symbol and (board[x+2][y+2]) == p1Symbol and (board[x+3][y+3]) == p1Symbol:
+                p1won()
+    for x in range(len(board)):
+        for y in range(3):
+            if (board[x][y]) == p2Symbol and (board[x+1][y+1]) == p2Symbol and (board[x+2][y+2]) == p2Symbol and (board[x+3][y+3]) == p2Symbol:
+                p2won()
+    
+def play():
+    while True:
+        p1move()
+        checkWin()
+        p2move()
+        checkWin()
+            
+makeBoard()
+p1Symbol, p2Symbol = chooseLetter()
+play()
